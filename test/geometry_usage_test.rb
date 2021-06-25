229a6d3f31ac7a4c678f73ea8c786097f01f2d56
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2021-06-25 12:21:05 +0200

    Add tests for CollectionMethods

--- test/geometry_usage_test.rb
+++ test/geometry_usage_test.rb
@@ -5,7 +5,7 @@ require_relative "../lib/rgeo-geojson"
 
 class GeometryUsageTest < Minitest::Test # :nodoc:
   def setup
-    @geo_factory = RGeo::Cartesian.simple_factory
+    @geo_factory = RGeo::Cartesian.factory
   end
 
   def test_feature_uses_method_missing
@@ -27,5 +27,47 @@ class GeometryUsageTest < Minitest::Test # :nodoc:
       rgeo_feature.as_text,
       rgeo_feature.geometry.as_text
     )
+  end
+
+  def test_collection_contains?
+    skip "`#contains?` is not defined in pure ruby" unless RGeo::Geos.capi_supported?
+
+    collection = polygon_and_point_collection
+
+    assert collection.contains? @geo_factory.point(5.0, 6.0)
+  end
+
+  def test_collection_intersects?
+    skip "`#intersects?`` is not defined in pure ruby" unless RGeo::Geos.capi_supported?
+
+    collection = polygon_and_point_collection
+
+    assert collection.intersects? @geo_factory.point(5.0, 6.0)
+  end
+
+  private
+
+  def polygon_and_point_collection
+    RGeo::GeoJSON.decode({
+      "type" => "FeatureCollection",
+      "features" => [
+        {
+          "type" => "Feature",
+          "geometry" => {
+            "type" => "Point",
+            "coordinates" => [10.0, 20.0],
+          },
+          "properties" => {},
+        },
+        {
+          "type" => "Feature",
+          "geometry" => {
+            "type" => "Polygon",
+            "coordinates" => [[[0.0, 0.0], [10.0, 10.0], [0.0, 10.0], [0.0, 0.0]]],
+          },
+          "properties" => {},
+        }
+      ]
+    }, geo_factory: @geo_factory)
   end
 end
